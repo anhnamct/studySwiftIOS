1c1c9867b7a3968ecb09416de038ac8b187cabe1
AnhNamCT <yeunganday@gmail.com>
2019-06-18 11:46:19 +0700

    UIImagePickerControllerDelegate

---
+++ StudyImage/StudyImage/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  StudyImage
+//
+//  Created by Kamui on 6/18/19.
+//  Copyright © 2019 Kamui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let chosenImage = info[.originalImage] as? UIImage {
+            imageView.image = chosenImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapGesteru(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+
+}
+
